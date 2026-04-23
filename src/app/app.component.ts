e40a579684d4193035de675f1625a53e8bc2bb38
Manoj Diddi <manoj.diddi@infor.com>
2026-04-23 16:12:06 +0530

    added signature animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { AboutComponent } from './components/about/about.component';
@@ -14,6 +15,7 @@ declare const AOS: any;
   selector: 'app-root',
   standalone: true,
   imports: [
+    CommonModule,
     HeaderComponent,
     HeroComponent,
     AboutComponent,
@@ -28,15 +30,35 @@ declare const AOS: any;
 })
 export class AppComponent implements OnInit {
   showScrollTop = false;
+  showSplash = true;
+  splashFading = false;
+
+  sparkles = Array.from({ length: 15 }, () => ({
+    left: (20 + Math.random() * 60) + '%',
+    top: (30 + Math.random() * 40) + '%',
+    delay: (1.5 + Math.random() * 1.5) + 's'
+  }));
+
+  signatureLetters = 'Manoj Kumar Diddi'.split('');
+  taglineLetters = 'Quality Engineer • Creative Soul'.split('');
+
+  bgLines = Array.from({ length: 8 }, () => ({
+    left: Math.random() * 100 + '%',
+    dur: (4 + Math.random() * 6) + 's',
+    delay: (Math.random() * 3) + 's'
+  }));
 
   ngOnInit(): void {
-    AOS.init({ duration: 600, easing: 'ease-in-out', once: true, mirror: false });
-    // Remove preloader
+    // Splash screen timing
+    setTimeout(() => { this.splashFading = true; }, 3000);
+    setTimeout(() => {
+      this.showSplash = false;
+      AOS.init({ duration: 600, easing: 'ease-in-out', once: true, mirror: false });
+    }, 3800);
+
+    // Remove old preloader
     const preloader = document.getElementById('preloader');
-    if (preloader) {
-      window.addEventListener('load', () => preloader.remove());
-      setTimeout(() => preloader.remove(), 2000);
-    }
+    if (preloader) preloader.remove();
   }
 
   @HostListener('window:scroll')
